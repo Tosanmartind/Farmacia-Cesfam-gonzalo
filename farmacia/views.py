c7ecc5573116b5416855634c451d36109f68628a
TuwuS <69693756+TuwuS@users.noreply.github.com>
2022-05-04 16:05:11 -0400

    Templates

--- farmacia/views.py
+++ farmacia/views.py
@@ -4,21 +4,21 @@ from django.shortcuts import render
 def login(request):
     return render(request, 'login/login.html')
 # Medico.
-#def consultaMedicamentos(request):
-#    return render(request, "medico/consulta_medicamento.html")
-#def recetas(request):
-#    return render(request, "medico/recetas.html")
-#def generarPrescripciones(request):
-#    return render(request, "medico/generar_prescripcion.html")
+def consultaMedicamentos(request):
+    return render(request, "medico/consulta_medicamento.html")
+def recetas(request):
+    return render(request, "medico/recetas.html")
+def generarPrescripciones(request):
+    return render(request, "medico/generar_prescripcion.html")
 # Farmacia.
-#def recepcionEntrega(request):
-#    return render(request, "farmacia/recepcion_entrega.html")
-#def recepcionFarmacia(request):
-#    return render(request, "farmacia/recepcion_farmacia.html")
+def recepcionEntrega(request):
+    return render(request, "farmacia/recepcion_entrega.html")
+def recepcionFarmacia(request):
+    return render(request, "farmacia/recepcion_farmacia.html")
 # Stock.
-#def adminInventario(request):
-#    return render(request, "stock/stockAdmin_inventario.html")
-#def agregarMedicamentos(request):
-#    return render(request, "stock/agregar_medicamentos.html")
-#def modificarMedicamentos(request):
-#    return render(request, "stock/modificar_medicamentos.html")
+def adminInventario(request):
+    return render(request, "stock/stockAdmin_inventario.html")
+def agregarMedicamentos(request):
+    return render(request, "stock/agregar_medicamentos.html")
+def modificarMedicamentos(request):
+    return render(request, "stock/modificar_medicamentos.html")
